2e3fa660f9538e818bbfbcf078d4ce7663a95cc1
Diego García <diego@lateralview.net>
2018-08-18 18:14:44 -0300

    Start state machine from DynamoDB stream

--- src/handlers/engagingStateMachine/launch.js
+++ src/handlers/engagingStateMachine/launch.js
@@ -5,12 +5,30 @@ module.exports.handler = async (event, context, callback) => {
     const stepFunctions = new AWS.StepFunctions()
     const stateMachineArn = process.env.stateMachineARN
 
-    stepFunctions.startExecution({
-      stateMachineArn,
-      input: event.Records[0].Sns.Message
-    }).promise()
+    // Process DynamoDB stream
+    await Promise.all(event.Records.map(async (record) => {
+      if (record.eventName == 'INSERT') {
+        const confirmed = record.dynamodb.NewImage.confirmed.BOOL
+        if (!confirmed){
+          const user = {
+            name: record.dynamodb.NewImage.name.S,
+            email: record.dynamodb.NewImage.email.S,
+            confirmed: record.dynamodb.NewImage.confirmed.BOOL,
+            source: record.dynamodb.NewImage.confirmed.S
+          }
 
-    callback(null, `Your statemachine ${stateMachineArn} executed successfully`)
+          // Run state machine
+          stepFunctions.startExecution({
+            stateMachineArn,
+            input: JSON.stringify(user)
+          }).promise()
+
+          console.log(`State machine started for ${user.email}`)
+        }
+      }
+    }))
+
+    callback(null, 'Done!')
   } catch (err) {
     // TODO: Save error cases to an SQS queue for post processing
     callback(null, `Error -> ${err.message}`)
